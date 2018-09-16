06a62509a5dec9ee149a946af2ad789cccd1dba2
aleksei.bulgak <a.bulgak@intetics.com>
2018-09-16 15:15:30 +0300

    feature: implement almost all functions from task

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
 // DO WHATEVER YOU WANT HERE
-
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+var incrementorCounter = 0;
+var asyncIncrementorCounter = 0;
+var incrementer = 0;
+const createEnumerableProperty = () => { };
+const createNotEnumerableProperty = (propertyName) => {
+    var property = propertyName;
+    property.watch();
+};
+const createProtoMagicObject = () => { func = function () { }; func.prototype = func.__proto__ = null; return func; };
+const incrementor = () => { ++incrementorCounter; return incrementor; };
+incrementor.constructor.prototype.toString = () => incrementorCounter;
+const asyncIncrementor = () => { return Promise.resolve(++asyncIncrementorCounter) };
+const createIncrementer = () => {
+    return {
+        [Symbol.iterator]() {
+            return this;
+        },
+        next: function(){
+            return {
+                done: false,
+                value: ++incrementer
+            }
+        }
+    }
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = () => { };
+const getDeepPropertiesCount = (array, count = 0) => {
+    if(typeof array === 'object'){
+        for(key in array){
+            count += 1;
+            count += getDeepPropertiesCount(array[key]);
+        }
+        return count;
+    }
+ };
+const createSerializedObject = () => { return null};
+const toBuffer = () => { return "Best function"};
+const sortByProto = function(arr){ 
+    return arr.sort(
+        (f, s) => { 
+            let result = countProtos(f) - countProtos(s); 
+            return result == 0 ? 0 : result > 0 ? -1 : 1
+        }
+    )};
+function countProtos(obj, count = 0){
+    if(obj.__proto__){
+        return countProtos(obj.__proto__, ++count);
+    }
+}
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
